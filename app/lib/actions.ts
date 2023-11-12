7ea0ce2245c0b94e76d595223e09c248d160d303
Kazuki1131 <onetsubyo@gmail.com>
2023-11-12 18:14:23 +0900

    feat: add validation to invoice

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -7,21 +7,45 @@ import { z } from 'zod';
 
 const InvoiceSchema = z.object({
 	id: z.string(),
-	customerId: z.string(),
-	amount: z.coerce.number(),
-	status: z.enum(['pending', 'paid']),
+	customerId: z.string({
+		invalid_type_error: 'お客様を選択してください。',
+	}),
+	amount: z.coerce
+		.number()
+		.gt(0, { message: '$0より大きい金額を入力してください。' }),
+	status: z.enum(['pending', 'paid'], {
+		invalid_type_error: '請求書のステータスを選択してください。',
+	}),
 	date: z.string(),
 });
 
 const CreateInvoice = InvoiceSchema.omit({ id: true, date: true });
 const UpdateInvoice = InvoiceSchema.omit({ date: true, id: true });
 
-export async function createInvoice(formData: FormData) {
-	const { customerId, amount, status } = CreateInvoice.parse({
+export type State = {
+	errors?: {
+		customerId?: string[];
+		amount?: string[];
+		status?: string[];
+	};
+	message?: string | null;
+};
+
+export async function createInvoice(prevState: State, formData: FormData) {
+	const validatedFields = CreateInvoice.safeParse({
 		customerId: formData.get('customerId'),
 		amount: formData.get('amount'),
 		status: formData.get('status'),
 	});
+
+	if (!validatedFields.success) {
+		return {
+			errors: validatedFields.error.flatten().fieldErrors,
+			message: 'フィールドがありません。請求書の作成に失敗しました',
+		};
+	}
+
+  const { customerId, amount, status } = validatedFields.data;
 	const amountInCents = amount * 100;
 	const date = new Date().toISOString().split('T')[0];
 
@@ -38,12 +62,21 @@ export async function createInvoice(formData: FormData) {
 	redirect('/dashboard/invoices');
 }
 
-export async function updateInvoice(id: string, formData: FormData) {
-	const { customerId, amount, status } = UpdateInvoice.parse({
+export async function updateInvoice(id: string, prevState: State, formData: FormData) {
+	const validatedFields = UpdateInvoice.safeParse({
 		customerId: formData.get('customerId'),
 		amount: formData.get('amount'),
 		status: formData.get('status'),
 	});
+
+	if (!validatedFields.success) {
+		return {
+			errors: validatedFields.error.flatten().fieldErrors,
+			message: 'フィールドがありません。請求書の作成に失敗しました',
+		};
+	}
+
+  const{ customerId, amount, status } = validatedFields.data;
 
 	const amountInCents = amount * 100;
 
